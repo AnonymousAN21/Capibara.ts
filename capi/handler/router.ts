70c8fb28adec27942709d58e05f6026fc31a0f83
AnonymousAN21 <reeve.tangel@gmail.com>
2025-07-26 17:24:05 +0800

    Router for grouping endpoint

--- capi/handler/router.ts
+++ capi/handler/router.ts
@@ -3,6 +3,8 @@ import https from "https";
 import { Response } from "../handler/response";
 import { Request } from "../handler/request";
 import { error } from "console";
+import { capi } from "../main/capi";
+import { Middleware, Handler } from "../main/capi";
 //to check the url pattern
 const pattern = /^\/[a-z0-9\-\/]*$/;
 /**
@@ -17,20 +19,49 @@ const pattern = /^\/[a-z0-9\-\/]*$/;
  * @ignore
  * still on progress
  */
-type Middleware = (req: Request, res: Response, next: () => void) => void;
-type Handler = (req: Request, res: Response) => void
 
-class router{
-    private group: {
-        routename: string,
-        router: {
-            method: string;
-            endpoint: string;
-            middlewares: Middleware[];
-            handler: Handler;
-        }[]
-    }[] = []
-    use(...middleware: [...Middleware[]]){
-        this.group
+export class Router {
+    private prefix: string;
+    private routes: {
+        method: string;
+        endpoint: string;
+        middlewares: Middleware[];
+        handler: Handler;
+    }[] = [];
+
+    constructor(prefix: string = "") {
+        this.prefix = prefix;
+    }
+
+    private register(method: string, endpoint: string, handlers: [...Middleware[], Handler]) {
+        const middlewares = handlers.slice(0, -1) as Middleware[];
+        const handler = handlers[handlers.length - 1] as Handler;
+
+        this.routes.push({
+            method,
+            endpoint: this.prefix + endpoint,
+            middlewares,
+            handler
+        });
+    }
+
+    getRoutes() {
+        return this.routes;
+    }
+
+    get(endpoint: string, ...handlers: [...Middleware[], Handler]) {
+        this.register("GET", endpoint, handlers);
+    }
+
+    post(endpoint: string, ...handlers: [...Middleware[], Handler]) {
+        this.register("POST", endpoint, handlers);
+    }
+
+    put(endpoint: string, ...handlers: [...Middleware[], Handler]) {
+        this.register("PUT", endpoint, handlers);
+    }
+
+    delete(endpoint: string, ...handlers: [...Middleware[], Handler]) {
+        this.register("DELETE", endpoint, handlers);
     }
 }
