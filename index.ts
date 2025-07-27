f4336b9c8c206d7a5ffbbe4983a5712d0823fbbd
AnonymousAN21 <reeve.tangel@gmail.com>
2025-07-27 17:10:56 +0800

    Export All new function

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 // index.ts
 import { capi } from "./capi/main/capi";
-import type { Middleware } from "./capi/main/capi";
+import { Middleware } from "./capi/main/capi";
 import { Response } from "./capi/handler/response";
 import { Request } from "./capi/handler/request";
-
-export default capi;
+import { Router } from "./capi/handler/router";
+import { cors } from "./capi/handler/cors";
+import { capyScrub } from "./capi/middleware/sanitize";
 
 // Optional named exports
-export type { Middleware };
-export { Response, Request };
+export { Response, Request, Middleware, capi, capyScrub, cors, Router};
